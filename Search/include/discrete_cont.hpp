884dc9187a15437646b1301aac810319d7bc981a
blackeye18 <panmavrom@gmail.com>
2021-12-16 09:38:18 -0800

    Search needs comments

---
+++ Search/include/discrete_cont.hpp
@@ -0,0 +1,17 @@
+/* Georgios Georgiou sdi1800220 - Panagiotis Mavrommatis sdi1800115 */
+#ifndef __discrete_cont__
+#define __discrete_cont__
+
+void stand_dev(vec* nvects,int no_of_vectors,int no_of_coordinates);
+int filtering(vec* nvectors,int no_of_coordinates,int no_of_vectors,int triads);
+int snapping_cont(vec* nvectors,int no_of_vectors,double delta);
+int padding_cont(vec* nvectors,int no_of_vectors,int no_of_coordinates,double delta);
+int mini_maxi_cont(vec* nvectors,int no_of_vectors,double delta);
+int key_cont(vec* nvectors,int no_of_vectors,int no_of_coordinates,double delta);
+vec* snapping(vec* nvectors,int no_of_coordinates,int no_of_vectors,double delta,vector<double> td1,vector<double>td2);
+
+
+
+
+
+#endif
